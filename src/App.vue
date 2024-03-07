d8e53b6ff508932bb326295b0af7de3afe50e2af
Fernando Barcala Rodriguez <fbarcalarodrigez@vector-software.com>
2024-03-07 22:11:26 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Weather from './components/Weather.vue'
+import { ref, watchEffect } from 'vue'
+</script>
+
+<script>
+export default {
+  name: "weather-list",
+  data() {
+    return {
+      weatherList: null
+    };
+  },
+  async created() {
+    const response = await fetch("http://localhost:5254/weatherforecast")
+    const weatherData = await response.json();
+    this.weatherList = weatherData;
+  }
+};
+</script>
+
+<template>
+  <div> Testing deployment </div>
+  <p>your weather: </p>
+  <p v-for="weather in weatherList">
+    <Weather :date="weather.date" :temperatureC="weather.temperatureC" :summary="weather.summary"
+      :temperatureF="weather.temperatureF" />
+  </p>
+</template>
---
+++ src/components/Weather.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+defineProps({
+  date: String,
+  summary: String,
+  temperatureC: Number,
+  temperatureF: Number,
+})
+
+</script>
+
+<template>
+  <h1>{{ date }}</h1>
+
+  <div>
+    <span>summary: </span>
+    <span>{{ summary }}</span>
+  </div>
+  <div>
+    <span>temperature: </span>
+    <span>{{ temperatureC }}</span>
+  </div>
+  <div>
+    <span>weird temperature (Fahrenheit): </span>
+    <span>{{ temperatureF }}</span>
+  </div>
+</template>
